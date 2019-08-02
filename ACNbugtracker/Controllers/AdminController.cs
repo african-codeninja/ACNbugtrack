b662b89dad7bc1b2c2701555666fe5d4584a054d
african-codeninja <51976848+african-codeninja@users.noreply.github.com>
2019-08-02 02:54:50 -0400

    Create AdminController.cs
    creating some manage users and roles views

---
+++ ACNbugtracker/Controllers/AdminController.cs
@@ -0,0 +1,55 @@
+﻿using ACNbugtracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ACNbugtracker.Controllers
+{
+    public class AdminController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Admin
+        public ActionResult UserIndex()
+        {
+            var users = db.Users.Select(userAttrib => new UserProfileViewModel
+            {
+                Id = userAttrib.Id,
+                FirstName = userAttrib.FirstName,
+                LastName = userAttrib.LastName,
+                DisplayName = userAttrib.DisplayName,
+                AvatarUrl = userAttrib.AvatarUrl,
+                Email = userAttrib.Email
+            }).ToList();
+
+            return View(users);
+        }
+
+        public ActionResult ManageUserRole()
+        {
+            return View();
+        }
+
+        public ActionResult ManageUsers()
+        {
+            return View();
+        }
+
+        public ActionResult ManageUserProjects()
+        {
+            return View();
+        }
+
+        public ActionResult ManageRoles()
+        {
+            return View();
+        }
+
+        public ActionResult ManageProjects()
+        {
+            return View();
+        }
+    }
+}
