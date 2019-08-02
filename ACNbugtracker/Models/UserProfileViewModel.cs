d0dafb5af6aabe8f2a776d408e083376ce7229cd
african-codeninja <51976848+african-codeninja@users.noreply.github.com>
2019-08-02 02:54:40 -0400

    Create UserProfileViewModel.cs
    creating some manage users and roles views

---
+++ ACNbugtracker/Models/UserProfileViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ACNbugtracker.Models
+{
+    public class UserProfileViewModel
+    {
+        public string Id { get; set; }
+
+        [MaxLength(50, ErrorMessage = "Cannot be greater than 50 Characters")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [MaxLength(20, ErrorMessage= "Cannot be greater than 20 Characters")]
+        [Display(Name = "Display Name")]
+        public string DisplayName { get; set; }
+
+        [Display(Name = "Avatar path")]
+        public string AvatarUrl { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        public HttpPostedFileBase Avatar { get; set; }
+    }
+}
