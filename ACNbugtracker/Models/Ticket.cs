95ddcef4869f953176e7c422347d6369310d013e
african-codeninja <51976848+african-codeninja@users.noreply.github.com>
2019-08-12 05:08:22 -0400

    Update Ticket.cs
    Editing Tickets view

--- ACNbugtracker/Models/Ticket.cs
+++ ACNbugtracker/Models/Ticket.cs
@@ -21,8 +21,9 @@ namespace ACNbugtracker.Models
         public string OwnerUserId { get; set; }
         [Display(Name ="Developer")]
         public string AssignedToUserId { get; set; }
-
+        [StringLength(30, ErrorMessage = "Must be between {2} and {1} characters long.", MinimumLength =6)]
         public string Title { get; set; }
+        [StringLength(400, ErrorMessage = "Must be between {2} and {1} characters long.", MinimumLength = 5)]
         public string Description { get; set; }
         public DateTime Created { get; set; }
         public DateTime? Updated { get; set; }
