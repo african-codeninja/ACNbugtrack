450b77d321326682262c4d7329fd746cf2c6fea4
african-codeninja <51976848+african-codeninja@users.noreply.github.com>
2019-08-15 22:54:15 -0400

    Update ProjectNotificationHelper.cs
    Sorting out issues with the template

--- ACNbugtracker/Helper/ProjectNotificationHelper.cs
+++ ACNbugtracker/Helper/ProjectNotificationHelper.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace ACNbugtracker.Helpers
+namespace ACNbugtracker.Helper
 {
     public class ProjectNotificationHelper
     {
