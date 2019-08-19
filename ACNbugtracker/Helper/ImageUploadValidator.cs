3e75b10d3c54e6de3b673ea0b0a8b268ac5f3064
african-codeninja <51976848+african-codeninja@users.noreply.github.com>
2019-08-19 11:57:14 -0400

    Update ImageUploadValidator.cs
    fine tuning my imageuploader helper class

--- ACNbugtracker/Helper/ImageUploadValidator.cs
+++ ACNbugtracker/Helper/ImageUploadValidator.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 
 namespace ACNbugtracker.Helper
 {
@@ -31,6 +33,68 @@ namespace ACNbugtracker.Helper
             catch
             {
                 return false;
+            }
+        }
+        public static bool IsValidAttachment(HttpPostedFileBase file)
+        {
+
+            try
+            {
+                if (file == null)
+                    return false;
+
+                if (file.ContentLength > 5 * 1024 * 1024 || file.ContentLength < 1024)
+                    return false;
+
+                var extValid = false;
+
+                foreach (var ext in WebConfigurationManager.AppSettings["AllowedAttachmentExtensions"].Split(','))
+                {
+                    if (Path.GetExtension(file.FileName) == ext)
+                    {
+                        extValid = true;
+                        break;
+                    }
+                        
+                }
+                return IsWebFriendlyImage(file) || extValid;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static string GetIconPath(string filePath)
+        {
+            switch(Path.GetExtension(filePath))
+            {
+               
+                case ".bmp":
+                case ".tif":
+                case ".ico":             
+                case ".jpeg":
+                case ".jpg":
+                case ".png":
+                    return filePath;                   
+                case ".pdf":
+                    return "/Images/Icons/pdf.png";
+                case ".doc":
+                    return "/Images/Icons/doc.png";
+                case ".html":
+                    return "/Images/Icons/html.png";              
+                case ".txt":
+                    return "/Images/Icons/txt.png";
+                case ".xls":
+                    return "/Images/Icons/xls.png";
+                case ".xlsx":
+                    return "/Images/Icons/xlsx.png";
+                case ".xml":
+                    return "/Images/Icons/xml.png";
+                case ".zip":
+                    return "/Images/Icons/zip.png";
+                default:
+                    return "/Images/Icons/other.png";
             }
         }
     }
