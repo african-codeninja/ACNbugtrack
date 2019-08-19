e1a9b5232408f94146e0a23598cf77309f60ba2a
african-codeninja <51976848+african-codeninja@users.noreply.github.com>
2019-08-19 11:51:13 -0400

    Create EmailModel.cs
    desingning my email models class to be used for sending emails

---
+++ ACNbugtracker/Models/EmailModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ACNbugtracker.Models
+{
+    public class EmailNotification
+    {
+        
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string Name { get; set; }
+        public bool IsChecked { get; set; }
+
+        public virtual ApplicationUser User { get; set; }
+    }
+}
