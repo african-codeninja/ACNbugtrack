e616d58d7432d4774d5abbd320ca0186775af9bf
african-codeninja <51976848+african-codeninja@users.noreply.github.com>
2019-08-19 11:57:39 -0400

    Create HistoryHelper.cs
    fine tuning my fine tuning my history helper class  helper class

---
+++ ACNbugtracker/Helper/HistoryHelper.cs
@@ -0,0 +1,41 @@
+﻿using ACNbugtracker.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace ACNbugtracker.Helper
+{
+    public class HistoryHelper
+    {
+        public UserManager<ApplicationUser> userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+
+        public static ApplicationDbContext db = new ApplicationDbContext();
+        public static void RecordHistory(Ticket oldTicket, Ticket newTicket)
+        {
+            foreach(var property in WebConfigurationManager.AppSettings["TrackedHistoryProperties"].Split(','))
+            {
+                var oldValue = oldTicket.GetType().GetProperty(property).GetValue(oldTicket, null).ToString();
+                var newValue = newTicket.GetType().GetProperty(property).GetValue(newTicket, null).ToString();
+
+                if(oldValue != newValue)
+                {
+                    var newHistory = new TicketHistory
+                    {
+                        UserId = HttpContext.Current.User.Identity.GetUserId(),
+                        Updated = (DateTime)newTicket.Updated,
+                        PropertyName = property,
+                        OldValue = oldValue,
+                        NewValue = newValue,
+                        TicketId = newTicket.Id
+                    };
+                    db.TicketHistories.Add(newHistory);
+                }
+            }
+            db.SaveChanges();
+        }
+    }
+}
