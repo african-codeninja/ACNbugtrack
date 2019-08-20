873314d89331a37a16c9baa72f64c3faa2081a1b
african-codeninja <51976848+african-codeninja@users.noreply.github.com>
2019-08-19 23:26:15 -0400

    Create NavHelper.cs
    stuck in an endless loop causing my app to crash jason figured out it was due to my common helper implementation

---
+++ ACNbugtracker/Helper/NavHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ACNbugtracker.Helper
+{
+    public class NavHelper: CommonHelper
+    {
+        //Help determine what links I can see in the side nav
+
+        public bool UserCanAddTickets()
+        {
+            //I am going to switch off the
+            switch (this.CurrentRole)
+            {
+                case SystemRole.Submitter:
+                    return true;
+                case SystemRole.Admin:
+                case SystemRole.ProjectManager:
+                case SystemRole.Developer:
+                default:
+                    return false;
+            }
+        }
+    }
+}
