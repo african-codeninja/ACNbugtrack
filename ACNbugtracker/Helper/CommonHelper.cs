a780d24dc713b61778527dd78adf69bca145193c
african-codeninja <51976848+african-codeninja@users.noreply.github.com>
2019-08-19 23:26:35 -0400

    Create CommonHelper.cs
    stuck in an endless loop causing my app to crash jason figured out it was due to my common helper implementation

---
+++ ACNbugtracker/Helper/CommonHelper.cs
@@ -0,0 +1,32 @@
+﻿using ACNbugtracker.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ACNbugtracker.Helper
+{
+    public abstract class CommonHelper
+    {
+        protected ApplicationDbContext db = new ApplicationDbContext();
+        protected UserRolesHelper RolesHelper = new UserRolesHelper();
+        protected ProjectsHelper ProjectsHelper = new ProjectsHelper();     
+        protected ApplicationUser CurrentUser = null;
+        protected SystemRole CurrentRole = SystemRole.None;
+
+        protected CommonHelper()
+        {
+            var userId = HttpContext.Current.User.Identity.GetUserId();
+            if (userId != null)
+                CurrentUser = db.Users.Find(userId);
+
+            var stringRole = RolesHelper.ListUserRoles(userId).FirstOrDefault();
+
+            if (!string.IsNullOrEmpty(stringRole))
+                CurrentRole = (SystemRole)Enum.Parse(typeof(SystemRole), stringRole);
+
+        }
+
+    }
+}
