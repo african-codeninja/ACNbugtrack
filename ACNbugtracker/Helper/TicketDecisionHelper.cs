eeb5bc6d3e30dd07acdc95922989c340586a927e
african-codeninja <51976848+african-codeninja@users.noreply.github.com>
2019-08-19 23:25:51 -0400

    Create TicketDecisionHelper.cs
    stuck in an endless loop causing my app to crash jason figured out it was due to my common helper implementation

---
+++ ACNbugtracker/Helper/TicketDecisionHelper.cs
@@ -0,0 +1,116 @@
+﻿using ACNbugtracker.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ACNbugtracker.Helper
+{
+    public class TicketDecisionHelper : CommonHelper
+    {
+        public bool TicketsDetailsViewableByUser(int ticketId)
+        {
+            var userId = HttpContext.Current.User.Identity.GetUserId();
+
+            var roleName = RolesHelper.ListUserRoles(userId).FirstOrDefault();
+
+            var systemRole = (SystemRole)Enum.Parse(typeof(SystemRole), roleName);
+
+            switch (systemRole)
+            {
+                case SystemRole.Admin:
+                    break;
+                case SystemRole.ProjectManager:
+                    break;
+                case SystemRole.Developer:
+                    break;
+                case SystemRole.Submitter:
+                    break;
+            }
+
+            return true;
+
+        }
+
+        public bool TicketIsEditableByUser(Ticket ticket)
+        {
+            var userId = HttpContext.Current.User.Identity.GetUserId();
+
+            var myRole = RolesHelper.ListUserRoles(userId).FirstOrDefault();
+
+            switch (myRole)
+            {
+                case "Developer":
+                    return (ticket.AssignedToUserId == userId);
+                case "Submitter":
+                    return (ticket.OwnerUserId == userId);
+                case "ProjectManager":
+                    var myProjects = ProjectsHelper.ListUserProjects(userId);
+                    foreach (var project in myProjects)
+                    {
+                        foreach (var projticket in project.Tickets)
+                        {
+                            if (projticket.Id == ticket.Id)
+                                return true;
+                        }
+                    }
+                    return false;
+
+                case "Admin":
+                    return true;
+                default:
+                    return false;
+                      
+            }
+        }
+
+        public bool TicketTypeIsEditableByUser(string userId, int ticketId)
+        {
+            userId = HttpContext.Current.User.Identity.GetUserId();
+
+            var roleName = RolesHelper.ListUserRoles(userId).FirstOrDefault();
+
+            var systemRole = (SystemRole)Enum.Parse(typeof(SystemRole), roleName);
+
+            switch (systemRole)
+            {
+                case SystemRole.Admin:
+                    break;
+                case SystemRole.ProjectManager:
+                    break;
+                case SystemRole.Developer:
+                    break;
+                case SystemRole.Submitter:
+                    break;
+            }
+
+            return true;
+        }
+
+        public bool TicketsTypeIsEditableByUser(string userId, int ticketId)
+        {
+            userId = HttpContext.Current.User.Identity.GetUserId();
+
+            var roleName = RolesHelper.ListUserRoles(userId).FirstOrDefault();
+
+            var systemRole = (SystemRole)Enum.Parse(typeof(SystemRole), roleName);
+
+            switch (systemRole)
+            {
+                case SystemRole.Admin:
+                    break;
+                case SystemRole.ProjectManager:
+                    break;
+                case SystemRole.Developer:
+                    break;
+                case SystemRole.Submitter:
+                    break;
+            }
+
+            return true;
+        }
+
+    }
+
+}
