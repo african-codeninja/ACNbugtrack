fc019bd39883c96383422fa87f41edd188534f49
african-codeninja <51976848+african-codeninja@users.noreply.github.com>
2019-08-21 09:29:14 -0400

    Update ProjectNotificationHelper.cs
    Creating more functionality in Bugtracker app

--- ACNbugtracker/Helper/ProjectNotificationHelper.cs
+++ ACNbugtracker/Helper/ProjectNotificationHelper.cs
@@ -9,7 +9,7 @@ namespace ACNbugtracker.Helper
 {
     public class ProjectNotificationHelper : CommonHelper
     {      
-        public void TriggerProjectAssignmentNotifications(List<ApplicationUser> oldProjectUsers, int projectId)
+        public static void TriggerProjectAssignmentNotifications(List<ApplicationUser> oldProjectUsers, int projectId)
         {
             //want to compare the old project users to the return value of users on project project.Id
             var newUsersOnProject = ProjectsHelper.UsersOnProject(projectId);
@@ -33,7 +33,7 @@ namespace ACNbugtracker.Helper
 
         }
 
-        public void AddProjectAssignmentNotification(int projectId, string newUser)
+        public static void AddProjectAssignmentNotification(int projectId, string newUser)
         {
             var properProjectName = db.Projects.FirstOrDefault(t => t.Id == projectId).Name;
             var newNotification = new ProjectNotification
@@ -48,7 +48,7 @@ namespace ACNbugtracker.Helper
             db.SaveChanges();
         }
 
-        public void AddProjectUnassignmentNotification(int projectId, string oldUser)
+        public static void AddProjectUnassignmentNotification(int projectId, string oldUser)
         {
             var properProjectName = db.Projects.FirstOrDefault(t => t.Id == projectId).Name;
             var oldNotification = new ProjectNotification
@@ -65,8 +65,8 @@ namespace ACNbugtracker.Helper
 
         public ICollection<ProjectNotification> ListUserProjectNotifications(string userId)
         {
-            var unreadNotifications = db.ProjectNotifications.Where(t => t.UserId == userId).ToList();
-            return (unreadNotifications);
+            var readNotifications = db.ProjectNotifications.Where(t => t.UserId == userId).ToList();
+            return (readNotifications);
         }
 
         public ICollection<ProjectNotification> ListUserUnreadProjectNotifications(string userId)
