cf977a95c21a9a4516ad8ee63d3776f7c044a874
african-codeninja <51976848+african-codeninja@users.noreply.github.com>
2019-08-26 11:09:48 -0400

    Create MorrisBarChart.cs
    creating a model class for the values that my charts are going to need to run

---
+++ ACNbugtracker/ChartViewModels/MorrisBarChart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ACNbugtracker.ChartViewModels
+{ 
+    public class MorrisBarData
+    {
+    public string label { get; set; }
+
+    public int value { get; set; }
+    }
+
+    public class FusionBarData
+    {
+    public string label { get; set; }
+
+    public string value { get; set; }
+    }
+}
