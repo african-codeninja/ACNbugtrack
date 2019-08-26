7173686bd6bcf635869eed56f689d31c0b51de59
african-codeninja <51976848+african-codeninja@users.noreply.github.com>
2019-08-26 11:05:36 -0400

    Update CommonHelper.cs
    modifying my common helper class to better  use of the inherent fuctions it is sharing

--- ACNbugtracker/Helper/CommonHelper.cs
+++ ACNbugtracker/Helper/CommonHelper.cs
@@ -9,23 +9,25 @@ namespace ACNbugtracker.Helper
 {
     public abstract class CommonHelper
     {
-        public static ApplicationDbContext db = new ApplicationDbContext();
-        public static UserRolesHelper RolesHelper = new UserRolesHelper();
-        public static ProjectsHelper ProjectsHelper = new ProjectsHelper();     
+        protected ApplicationDbContext db = new ApplicationDbContext();
+        protected UserRolesHelper RolesHelper = new UserRolesHelper();
+        protected ProjectsHelper ProjectsHelper = new ProjectsHelper();
         protected ApplicationUser CurrentUser = null;
+        protected String CurrentRoles = "";
         protected SystemRole CurrentRole = SystemRole.None;
 
         protected CommonHelper()
         {
             var userId = HttpContext.Current.User.Identity.GetUserId();
-            if (userId != null)
-                CurrentUser = db.Users.Find(userId);
+            //if (userId != null)
+            //    CurrentUser = db.Users.Find(userId);
 
-            var stringRole = RolesHelper.ListUserRoles(userId).FirstOrDefault();
+            //var stringRole = RolesHelper.ListUserRoles(userId).FirstOrDefault();
 
-            if (!string.IsNullOrEmpty(stringRole))
-                CurrentRole = (SystemRole)Enum.Parse(typeof(SystemRole), stringRole);
-
+            //if (!string.IsNullOrEmpty(stringRole))
+            //    CurrentRole = (SystemRole)Enum.Parse(typeof(SystemRole), stringRole);
+            CurrentUser = db.Users.Find(userId);
+            CurrentRoles = RolesHelper.ListUserRoles(CurrentUser.Id).FirstOrDefault();
         }
 
     }
