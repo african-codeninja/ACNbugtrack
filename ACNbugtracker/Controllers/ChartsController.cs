72d29b53ae9f805392f5297d039a9c4f54ffe3bb
african-codeninja <51976848+african-codeninja@users.noreply.github.com>
2019-08-26 11:08:09 -0400

    Create ChartsController.cs
    creating a charts controller to implement charts on my home dash board

---
+++ ACNbugtracker/Controllers/ChartsController.cs
@@ -0,0 +1,41 @@
+﻿using ACNbugtracker.ChartViewModels;
+using ACNbugtracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ACNbugtracker.Controllers
+{
+    public class ChartsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Charts
+        public JsonResult GetJsonMorrisData()
+        {
+            var dataSet = new List<MorrisBarData>();
+
+            foreach(var ticketStatus in db.TicketStatuses.ToList())
+            {
+                var value = db.TicketStatuses.Find(ticketStatus.Id).Tickets.Count();
+                dataSet.Add(new MorrisBarData { label = ticketStatus.Name, value = value });               
+            }
+            return Json(dataSet);
+        }
+
+        public JsonResult GetJsonFusionData()
+        {
+            var dataSet = new List<MorrisBarData>();
+
+            foreach (var ticketStatus in db.TicketStatuses.ToList())
+            {
+                var value = db.TicketStatuses.Find(ticketStatus.Id).Tickets.Count();
+                dataSet.Add(new MorrisBarData { label = ticketStatus.Name, value = value });
+            }
+            return Json(dataSet);
+        }
+    }
+
+    
+}
