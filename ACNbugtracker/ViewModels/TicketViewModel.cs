a7b06db8d6f75e2884b2c33098188945849069eb
african-codeninja <51976848+african-codeninja@users.noreply.github.com>
2019-08-29 02:28:02 -0400

    Create TicketViewModel.cs
    ticket view models

---
+++ ACNbugtracker/ViewModels/TicketViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ACNbugtracker.ViewModels
+{
+    public class TicketViewModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int TicketPriorityId { get; set; }
+
+        public IEnumerable<SelectListItem> CurrentPriority { get; set; }
+
+        public DateTime Created { get; set; }
+    }
+}
