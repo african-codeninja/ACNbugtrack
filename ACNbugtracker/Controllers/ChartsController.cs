453af365ce3272849fc1c9317931df8d1fe3f67d
african-codeninja <51976848+african-codeninja@users.noreply.github.com>
2019-08-29 14:56:43 -0400

    Update ChartsController.cs
    Finally will be implementing fusion 3d js charts and see how it turns out

--- ACNbugtracker/Controllers/ChartsController.cs
+++ ACNbugtracker/Controllers/ChartsController.cs
@@ -24,14 +24,50 @@ namespace ACNbugtracker.Controllers
             return Json(dataSet);
         }
 
+        public JsonResult GetJsonFusionDataTypes()      
+        {
+            var dataSet = new List<FusionBarData>();
+
+            foreach (var tickettypes in db.TicketTypes.ToList())
+            {
+                var value = db.TicketStatuses.Find(tickettypes.Id).Tickets.Count();
+                dataSet.Add(new FusionBarData { label = tickettypes.Name, value = value.ToString()});
+            }
+            return Json(dataSet);
+        }
+
         public JsonResult GetJsonFusionData()
         {
-            var dataSet = new List<MorrisBarData>();
+            var dataSet = new List<FusionBarData>();
 
             foreach (var ticketStatus in db.TicketStatuses.ToList())
             {
                 var value = db.TicketStatuses.Find(ticketStatus.Id).Tickets.Count();
-                dataSet.Add(new MorrisBarData { label = ticketStatus.Name, value = value });
+                dataSet.Add(new FusionBarData { label = ticketStatus.Name, value = value.ToString()});
+            }
+            return Json(dataSet);
+        }
+
+        public JsonResult GetJsonFusionProjectUsers()
+        {
+            var dataSet = new List<FusionBarData>();
+
+            foreach (var projects in db.Projects.ToList())
+            {
+                var value = db.Projects.Find(projects.Id).Users.Count();
+                dataSet.Add(new FusionBarData { label = projects.Name, value = value.ToString()});
+            }
+            return Json(dataSet);
+        }
+
+        public JsonResult GetJsonFusionDataRoles()
+        {
+            var dataSet = new List<FusionBarData>();
+
+            foreach (var roles in db.Roles.ToList())
+            {
+                var value = db.Roles.Find(roles.Id).Users.Count();
+                dataSet.Add(new FusionBarData { label = roles.Name, value = value.ToString() });
             }
             return Json(dataSet);
         }
