ade9f56b8bf2538647ed9ceae206de0a3039bbd5
african-codeninja <51976848+african-codeninja@users.noreply.github.com>
2019-08-29 02:31:41 -0400

    Create Utilities.cs
    a utilities helper class

---
+++ ACNbugtracker/Helper/Utilities.cs
@@ -0,0 +1,33 @@
+﻿using ACNbugtracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ACNbugtracker.Helper
+{
+    public class Utilities
+    {
+        private static ApplicationDbContext db = new ApplicationDbContext();
+
+        public static string MakeReadable(string property, string value)
+        {
+            switch (property)
+            {
+                case "TicketStatusId":
+                    return db.TicketStatuses.Find(Convert.ToInt32(value)).Name;
+                case "TicketPriorityId":
+                    return db.TicketPriorities.Find(Convert.ToInt32(value)).Name;
+                case "TicketTypeId":
+                    return db.TicketTypes.Find(Convert.ToInt32(value)).Name;
+                case "AssignedToUserId":
+                case "OwnerUserId":
+                    return db.Users.Find(value).FullName;
+                default:
+                    return value;
+
+            }
+
+        }
+    }
+}
