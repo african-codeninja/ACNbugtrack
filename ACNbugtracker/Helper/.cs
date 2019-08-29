ee485878b8adcb93e874b1c00c22055cb11c7069
african-codeninja <51976848+african-codeninja@users.noreply.github.com>
2019-08-29 02:33:55 -0400

    Create .cs
    creating helper classes

---
+++ ACNbugtracker/Helper/.cs
@@ -0,0 +1,30 @@
+﻿using ACNbugtracker.Models;
+using ACNbugtracker.Helper;
+using ACNbugtracker.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ACNbugtracker.Helper
+{
+    public class LinkHelper : CommonHelper
+    {
+        public bool UserCanEditTicket(Ticket ticket)
+        {
+            switch (this.CurrentRoles)
+            {
+                case "Admin":
+                    return true;
+                case "ProjectManager":
+                    return CurrentUser.Projects.SelectMany(t => t.Tickets).Select(t => t.Id).Contains(ticket.Id);
+                case "Developer":
+                    return ticket.AssignedToUserId == CurrentUser.Id;
+                case "Submitter":
+                    return ticket.OwnerUserId == CurrentUser.Id;
+                default:
+                    return false;
+            }
+        }
+    }
+}
