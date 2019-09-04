f90e71a15cc8584e77738776d3d0a91ffc5aad5b
african-codeninja <51976848+african-codeninja@users.noreply.github.com>
2019-09-04 03:35:59 -0400

    Update UserProfileViewModel.cs

--- ACNbugtracker/Models/UserProfileViewModel.cs
+++ ACNbugtracker/Models/UserProfileViewModel.cs
@@ -1,30 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace ACNbugtracker.Models
+namespace ACNbugtracker.ViewModels
 {
     public class UserProfileViewModel
     {
         public string Id { get; set; }
 
-        [MaxLength(50, ErrorMessage = "Cannot be greater than 50 Characters")]
+        [Required]
+        [MaxLength(40, ErrorMessage = "First Name cannot be greater than 40 characters")]
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(40, ErrorMessage = "Last Name cannot be greater than 40 characters")]
         [Display(Name = "Last Name")]
         public string LastName { get; set; }
-        [MaxLength(20, ErrorMessage= "Cannot be greater than 20 Characters")]
+
+        [MaxLength(20, ErrorMessage = "Display Name cannot be greater than 20 characters")]
         [Display(Name = "Display Name")]
         public string DisplayName { get; set; }
+
+        public string PhoneNumber { get; set; }
 
         [Display(Name = "Avatar path")]
         public string AvatarUrl { get; set; }
 
         [Required]
         [EmailAddress]
-        [Display(Name = "Email")]
         public string Email { get; set; }
 
         public HttpPostedFileBase Avatar { get; set; }
