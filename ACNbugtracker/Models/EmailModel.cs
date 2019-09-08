58698895c24b2b19c5efea3de990756e18409f55
african-codeninja <51976848+african-codeninja@users.noreply.github.com>
2019-09-08 00:35:07 -0400

    Update EmailModel.cs
    adding some comments to the models class

--- ACNbugtracker/Models/EmailModel.cs
+++ ACNbugtracker/Models/EmailModel.cs
@@ -7,12 +7,13 @@ namespace ACNbugtracker.Models
 {
     public class EmailNotification
     {
-        
+        //Primary Key
         public int Id { get; set; }
         public string UserId { get; set; }
         public string Name { get; set; }
         public bool IsChecked { get; set; }
 
+        //Navigation to parent
         public virtual ApplicationUser User { get; set; }
     }
 }
