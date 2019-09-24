6cb7068392660f0e1d23cd19bd155e41e72068d7
african-codeninja <51976848+african-codeninja@users.noreply.github.com>
2019-09-24 04:26:04 -0400

    Create ProfileViewModel.cs
    creating a profileview model

---
+++ ACNbugtracker/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ACNbugtracker.Models;
+
+namespace ACNbugtracker.ViewModels
+{
+    public class ProfileViewModel
+    {
+        public IndexViewModel IndexViewModel { get; set; }
+        public UserViewModel UserViewModel { get; set; }
+        public ChangePasswordViewModel ChangePasswordViewModel { get; set; }
+    }
+}
